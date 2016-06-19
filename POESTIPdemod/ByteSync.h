038671cfd61a54e6bd32c0b3288828d96694236a
Nebarnix <nebarotype@gmail.com>
2016-06-18 23:46:04 -0700

    Delete ByteSync.h

--- POESTIPdemod/ByteSync.h
+++
@@ -1,4 +0,0 @@
-#ifndef BYTESYNC_H
-#define BYTESYNC_H
-int ByteSyncOnSyncword(unsigned char *bitStreamIn, double *bitStreamInTime, unsigned long nSamples,  char *syncWord, unsigned int syncWordLength, FILE *minorFrameFile);
-#endif
