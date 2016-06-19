65756fc631d56280e5d03e2a55d341dfcfa6a04b
Nebarnix <nebarotype@gmail.com>
2016-06-18 23:46:49 -0700

    Delete MMClockRecovery.h

--- POESTIPdemod/MMClockRecovery.h
+++
@@ -1,5 +0,0 @@
-#ifndef MMCLOCKRECOVERY_H
-#define MMCLOCKRECOVERY_H
-unsigned long MMClockRecovery(double *dataStreamIn,double *dataStreamInTime, unsigned long numSamples, double *dataStreamOut, int Fs, double stepRange, double kp);
-int sign(double x);
-#endif
