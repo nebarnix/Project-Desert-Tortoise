afccdc541aa4c23fb1f1dd679a046b8379ded577
Nebarnix <nebarotype@gmail.com>
2016-06-18 23:42:44 -0700

    Delete LowPassFilter.h

--- ARGOSdemodPortAudio/LowPassFilter.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOWPASSFILTER_H
-#define LOWPASSFILTER_H
-
-void LowPassFilter(double *dataStream, unsigned long nSamples, double *filterCoeffs, int N);
-int MakeLPFIR(double *h, int N, double Fc, double Fs);
-#endif
