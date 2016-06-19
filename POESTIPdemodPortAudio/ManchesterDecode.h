440a496dc093c562b26d3c7645537151f7d6e371
Nebarnix <nebarotype@gmail.com>
2016-06-18 23:53:37 -0700

    Delete ManchesterDecode.h

--- POESTIPdemodPortAudio/ManchesterDecode.h
+++
@@ -1,4 +0,0 @@
-#ifndef MANCHESTERDECODE_H
-#define MANCHESTERDECODE_H
-unsigned long ManchesterDecode(double *dataStreamIn, double *dataStreamInTime, unsigned long nSymbols, unsigned char *bitStream, double resyncThreshold);
-#endif
