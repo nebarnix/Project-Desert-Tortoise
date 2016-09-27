24b7300b58fd941750fe1242e0f4e883fbdf1056
Nebarnix <nebarotype@gmail.com>
2016-09-27 00:27:06 -0700

    Add files via upload
    GLOBAL
    Sept 25, 2016
    -Reversed idea to use bytesync.c as a common library due to the fact that packetized and nonpacketized streams have a different needs. Perhaps these could be POESbytesyc and ARGOSbytesync to avoid updating the two versions each time?
    -included stdint.h to wave.c to cover int16_t data_in_channel because mingw suddenly stopped compiling it	
    -IDEA! Unlock (or even reset) the PLL if the quality estimator is very low for a long time-- we may have a false lock on a nearby signal
    
    POES POST
    Sept 25, 2016
    -Restored archived bytesync.c
    --Reverted two passed variables that were needed to decode universal streams (length and offset) -- now specific again
    --Syncrhonized updated 'long idx' to 'unsigned long idx'
    --Synchronized 'memset(historyBufferCirc, '.', sizeof(char) * syncWordLength);' to 'memset(historyBufferCirc, 48, sizeof(char) * syncWordLength);'
    --Syncrhonized addition of 'i' to inverted frames
    -Fixed typo: WAS DSP_PLL_LOCK_ALPA  SB DSP_PLL_LOCK_ALPHA
    --S

--- ARGOSdemod/main.c
+++ ARGOSdemod/main.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <complex.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
-//#include <koolplot.h>
+#include <time.h>
+#include <conio.h>
 #include "../common/wave.h"
 #include "../common/AGC.h"
 #include "../common/CarrierTrackPLL.h"
@@ -12,17 +14,32 @@
 //#include "../common/MMClockRecovery.h"
 #include "../common/GardenerClockRecovery.h"
 #include "../common/ManchesterDecode.h"
-#include "../common/ByteSync.h"
+#include "ByteSync.h"
+
+#define DEFAULT_CHUNKSIZE  (1000)
+
+#define DSP_MAX_CARRIER_DEVIATION   (550.0) //was (550.0)
+#define DSP_PLL_LOCK_THRESH         (0.1) //was (1.00) //AR0.1) (doesn't really do anything if the track and acquire gains are the same)
+#define DSP_PLL_LOCK_ALPHA          (0.004)
+#define DSP_PLL_ACQ_GAIN            (0.015) //0.0015 //was (0.015) 
+#define DSP_PLL_TRCK_GAIN           (0.015) // 0.0015 //was (0.015)
+#define DSP_SQLCH_THRESH            (0.15) //was (0.25) //was (0.15) //AR0.3
 
 #define DSP_GDNR_ERR_LIM            (0.1) //was 0.1
 #define DSP_GDNR_GAIN               (3.0) //was 2.5
 #define DSP_BAUD                    (400*2)
+#define DSP_MCHSTR_RESYNC_LVL       (0.5) //was (0.5)
 #define DSP_AGC_ATCK_RATE           (1e-1)//(0.5e-1) //was (1e-1) //attack is when gain is INCREASING (weak signal)
 #define DSP_AGC_DCY_RATE            (2e-1) //was (1e-1) //decay is when the gain is REDUCING (strong signal)
-#define DSP_PLL_LOCK_ALPA           (0.004)
+
+#define DSP_AGCC_GAIN               (0.001) //(0.0005) //was (0.001) //AR0.0015
+#define DSP_LPF_FC                  (700) //(was (700)
+#define DSP_LPF_ORDER               (50) //was (50)
 
 #define TRUE 1
 #define FALSE 0
+
+//#define RAW_OUTPUT_FILES
 
 //int spaceCraftID, dayNum, 
 
@@ -49,18 +66,23 @@ const char *get_filename_ext(const char *filename)
    
 int main(int argc, char **argv) 
    {
-   FILE *inFilePtr=NULL;
-   FILE *rawOutFilePtr=NULL;
-   FILE *minorFrameFile=NULL;
-   
+   //Wave variable
    HEADER header;
    
-   unsigned long chunkSize = 10000, nSamples, i=0, idx, nSymbols, nBits, totalSymbols=0, totalBits=0, totalSamples=0;
+   //Files we will use
+   FILE *inFilePtr=NULL;
+   FILE *minorFrameFile=NULL;
    
-   double *dataStreamReal, *dataStreamSymbols, *lockSignalStream;
+   #ifdef RAW_OUTPUT_FILES
+      FILE *rawOutFilePtr=NULL;      
+   #endif
+   
+   unsigned long chunkSize = DEFAULT_CHUNKSIZE, nSamples, i=0, idx, nSymbols, nBits, totalSymbols=0, totalBits=0, totalSamples=0;
+   
+   double *dataStreamReal=NULL, *dataStreamSymbols=NULL, *lockSignalStream=NULL;
    double Fs;
    double LPF_Fc;   
-   double avgPhase, percentComplete=0;
+   double averagePhase, percentComplete=0;
    double normFactor=0;
    
    double *filterCoeffs=NULL, *waveDataTime=NULL;
@@ -72,7 +94,10 @@ int main(int argc, char **argv)
    
    unsigned char *dataStreamBits=NULL;
    char *inFileName=NULL;
-   //char *cvalue = NULL;   
+   char outFileName[100];
+   
+   const char *build_date = __DATE__;
+   printf("Project Desert Tortoise: Realtime ARGOS Demodulator by Nebarnix.\nBuild date: %s\n",build_date);
    
    while ((c = getopt (argc, argv, "n:c:")) != -1)
       {
@@ -111,7 +136,9 @@ int main(int argc, char **argv)
          }
       }
    printf("Using %ld chunkSize\n",chunkSize);
-   LPF_Order = 50;
+   LPF_Order = DSP_LPF_ORDER;
+   
+   //Allocate the memory we will need
    filterCoeffs = malloc(sizeof(double) * LPF_Order);   
    inFileName = (char*) malloc(sizeof(char) * 1024);      
    waveData = (double complex*) malloc(sizeof(double complex) * chunkSize);      
@@ -138,21 +165,26 @@ int main(int argc, char **argv)
    char cwd[1024];
    if (getcwd(cwd, sizeof(cwd)) != NULL) 
       {      
-      strcpy(inFileName, argv[optind]);
-      
+            
       // get inFileName from command line
       if (argc < 2)
          {
          printf("No wave file specified\n");
          return 1;
          }
+      strcpy(inFileName, argv[optind]);
       printf("%s\n", inFileName);
       }
 
    // open files
    printf("Opening IO files..\n");
    inFilePtr = fopen(inFileName, "rb");
-   minorFrameFile = fopen("packets.txt", "w");
+   
+   time_t t = time(NULL);
+   struct tm tm = *localtime(&t);
+   //printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+   snprintf(outFileName, 100,"packets_%4d%02d%02d_%02d%02d%02d.txt",tm.tm_year + 1900,tm.tm_mon + 1,tm.tm_mday,tm.tm_hour, tm.tm_min, tm.tm_sec);
+   minorFrameFile = fopen(outFileName, "w");
   
    if (minorFrameFile == NULL ||
       inFilePtr == NULL)
@@ -161,14 +193,15 @@ int main(int argc, char **argv)
       exit(1);
       }
       
-   
+   #ifdef RAW_OUTPUT_FILES
    rawOutFilePtr = fopen("output.raw", "wb");
    if (rawOutFilePtr == NULL)
       {
       printf("Error opening output file\n");
       exit(1);
       }
-      
+   #endif
+   
    if(strcasecmp(get_filename_ext(inFileName),"wav") == 0)
       header = ReadWavHeader(inFilePtr);
    else
@@ -184,7 +217,7 @@ int main(int argc, char **argv)
    
    printf("Sample Rate %.2fKHz and %d bits per sample. Total samples %ld\n", Fs/1000.0, header.bits_per_sample ,num_samples);
 
-   LPF_Fc = 700;   
+   LPF_Fc = DSP_LPF_FC;   
    MakeLPFIR(filterCoeffs, LPF_Order, LPF_Fc, Fs, 1);
    while(!feof(inFilePtr))
       { 
@@ -198,27 +231,27 @@ int main(int argc, char **argv)
          }
          
       i+=nSamples;
-      /*
-      //normalize
-      for(idx=0; idx < nSamples; idx++)
-         {
-          waveData[idx] *= normFactor;         
-         }*/
-      NormalizingAGCC(waveData, nSamples, normFactor, 0.001);
+      
+      NormalizingAGCC(waveData, nSamples, normFactor, DSP_AGCC_GAIN);
             
-      avgPhase = CarrierTrackPLL(waveData, dataStreamReal, lockSignalStream, nSamples, Fs, 550, 1, DSP_PLL_LOCK_ALPHA, 0.015, 0.015);      
-      (void)avgPhase;      
+      averagePhase = CarrierTrackPLL(waveData, dataStreamReal, lockSignalStream, nSamples, Fs, DSP_MAX_CARRIER_DEVIATION, DSP_PLL_LOCK_THRESH, DSP_PLL_LOCK_ALPHA, DSP_PLL_ACQ_GAIN, DSP_PLL_TRCK_GAIN);      
+      (void)averagePhase;      
       LowPassFilter(dataStreamReal, nSamples, filterCoeffs, LPF_Order);      
       NormalizingAGC(dataStreamReal, nSamples, DSP_AGC_ATCK_RATE, DSP_AGC_DCY_RATE);      
-      //Squelch(dataStreamReal, lockSignalStream, nSamples, 0.25);
+      
+      #ifdef RAW_OUTPUT_FILES
+         fwrite(dataStreamReal, sizeof(double), nSamples,rawOutFilePtr);
+      #endif
+      
+      Squelch(dataStreamReal, lockSignalStream, nSamples, DSP_SQLCH_THRESH);
       //nSymbols = MMClockRecovery(dataStreamReal, waveDataTime, nSamples, dataStreamSymbols, Fs, 3, 0.15);
       nSymbols = GardenerClockRecovery(dataStreamReal, waveDataTime, nSamples, dataStreamSymbols, Fs, DSP_BAUD, DSP_GDNR_ERR_LIM, DSP_GDNR_GAIN);
       
       //fwrite(dataStreamReal, sizeof(double), nSamples,rawOutFilePtr);
       
-      nBits = ManchesterDecode(dataStreamSymbols, waveDataTime, nSymbols, dataStreamBits, 0.5);
+      nBits = ManchesterDecode(dataStreamSymbols, waveDataTime, nSymbols, dataStreamBits, DSP_MCHSTR_RESYNC_LVL);
       // fwrite(dataStreamBits, sizeof(char), nBits,rawOutFilePtr);
-      nFrames = ByteSyncOnSyncword(dataStreamBits, waveDataTime, nBits, "0001011110000", 13, 7, 0, minorFrameFile);      
+      nFrames = FindSyncWords(dataStreamBits, waveDataTime, nBits, "0001011110000", 13, minorFrameFile);      
       
       totalBits += nBits;
       totalFrames += nFrames;
@@ -229,7 +262,7 @@ int main(int argc, char **argv)
          percentComplete = ((double)( i) / num_samples)*100.0;
          printf("\r");
          //printf("\n");
-         printf("%0.1f%% %0.3f Ks : %0.1f Sec: %ld Sym : %ld Bits : %d Frames", ((double)( i) / num_samples)*100.0,(totalSamples)/1000.0, waveDataTime[0], totalSymbols, totalBits, totalFrames);
+         printf("%0.1f%% %0.3f Ks : %0.1f Sec: %ld Sym : %ld Bits : %d Packets", ((double)( i) / num_samples)*100.0,(totalSamples)/1000.0, waveDataTime[0], totalSymbols, totalBits, totalFrames);
          }
       
       
@@ -244,7 +277,9 @@ int main(int argc, char **argv)
    
    //printf("\nChecksum1=%X Checksum2=%X Checksum3=%X", CheckSum1,CheckSum2,CheckSum3);
    printf("\nAll done! Closing files and exiting.\nENJOY YOUR BITS AND HAVE A NICE DAY\n");
-   //fclose(rawOutFilePtr);
+   #ifdef RAW_OUTPUT_FILES
+      fclose(rawOutFilePtr);
+   #endif
    fclose(inFilePtr);
    fclose(minorFrameFile);
    
