2b76299afdede14322396c0496fe123a1bc2d3b5
Nebarnix <nebarotype@gmail.com>
2017-04-06 07:34:04 -0700

    Update daytimeDecode.m
    Updated the T0 error estimation to use a mode of the nearest ms times and validate using a 100mS threshold from this value. 
    
    This should give more consistent results!

--- standalone_matlab/Functionized/daytimeDecode.m
+++ standalone_matlab/Functionized/daytimeDecode.m
@@ -20,29 +20,52 @@ for frame=1:numel(minorFrameID)
         
         %check if the counter is more than the number of ms per day (bad data)
         if((bitshift(bitand(minorFrames(frame,9+1),bin2dec('111')),24) + bitshift(minorFrames(frame,10+1),16) + bitshift(minorFrames(frame,11+1),8) + minorFrames(frame,12+1)) < 86400000)
-            dayMSeconds(idx) = (bitshift(bitand(minorFrames(frame,9+1),bin2dec('111')),24) + bitshift(minorFrames(frame,10+1),16) + bitshift(minorFrames(frame,11+1),8) + minorFrames(frame,12+1));
-            fprintf([num2str(frameTime(frame,1)) ' Local Seconds is ' num2str(dayMSeconds(idx)/1000.0) ' Spacecraft Day Seconds' ]);
+            dayMSeconds(idx) = (bitshift(bitand(minorFrames(frame,9+1),bin2dec('111')),24) + bitshift(minorFrames(frame,10+1),16) + bitshift(minorFrames(frame,11+1),8) + minorFrames(frame,12+1));            
+            T0MSeconds(idx) = dayMSeconds(idx)-frameTime(frame,1)*1000;
+        else
+            dayMSeconds(idx) = -1;
+            T0MSeconds(idx) = -1;
+        end        
+        idx = idx + 1;
+        %fprintf([num2str(frameTime(frame,1)) ':' num2str(dec2bin(minorFrames(frame,9+1),8)) ' ' num2str(dec2bin(minorFrames(frame,10+1),8)) ' ' num2str(dec2bin(minorFrames(frame,11+1),8)) ' ' num2str(dec2bin(minorFrames(frame,12+1),8)) '\n']);
+        %fprintf([num2str(day) ' ' num2str(dec2hex(minorFrames(frame,8+1))) ' ' num2str(dec2hex(minorFrames(frame,9+1))) '\n\n']);        
+    end
+end
+
+if(numel(dayMSeconds)>1)
+   
+   T0BestGuess = mode(round(T0MSeconds(T0MSeconds>0)));
+   T0Threshold = 100; %100mS of jitter is MORE than enough margin!
+   hour = floor(T0BestGuess/(1000*60*60));
+   minute = floor((T0BestGuess/(1000*60*60) - hour)*60);
+   seconds = (((T0BestGuess/(1000*60*60) - hour)*60) - minute) *60;
+   
+   fprintf(['T0 Best Guess: ' num2str(T0BestGuess) ' which is ' num2str(hour) ':' num2str(minute) ':' num2str(seconds) '\n']);
+end
+
+for idx=1:numel(dayMSeconds)  
+    if(dayMSeconds(idx) > 0)
+        fprintf([num2str(frameTime(frame,1)) ' Local Seconds is ' num2str(dayMSeconds(idx)/1000.0) ' Spacecraft Day Seconds' ]);
             
             hour = floor(dayMSeconds(idx)/(1000*60*60));
             minute = floor((dayMSeconds(idx)/(1000*60*60) - hour)*60);
             seconds = (((dayMSeconds(idx)/(1000*60*60) - hour)*60) - minute) *60;
             fprintf([' which is ' num2str(hour) ':' num2str(minute) ':' num2str(seconds)]);
             
-            if idx > 1 && dayMSeconds(idx) <= dayMSeconds(idx-1)
-                fprintf(' ...but this might be an error');
-            else
-                hour = floor((dayMSeconds(idx)-frameTime(frame,1)*1000)/(1000*60*60));
-                minute = floor(((dayMSeconds(idx)-frameTime(frame,1)*1000)/(1000*60*60) - hour)*60);
-                seconds = ((((dayMSeconds(idx)-frameTime(frame,1)*1000)/(1000*60*60) - hour)*60) - minute) *60;
-                fprintf(['    t(0) => ' num2str(hour) ':' num2str(minute) ':' num2str(seconds)]);
+            %better way to do this would be to store the T0's then take the
+            %mode and look for T0's that are off by more than some
+            %threshold value
+            %if idx > 1 && dayMSeconds(idx) <= dayMSeconds(idx-1)
+            
+            hour = floor((T0MSeconds(idx))/(1000*60*60));
+            minute = floor(((T0MSeconds(idx))/(1000*60*60) - hour)*60);
+            seconds = ((((T0MSeconds(idx))/(1000*60*60) - hour)*60) - minute) *60;
+            fprintf(['    t(0) => ' num2str(hour) ':' num2str(minute) ':' num2str(seconds) ' or ' num2str(T0MSeconds(idx))]);
+            
+            if((T0MSeconds(idx)+T0Threshold) < T0BestGuess || (T0MSeconds(idx)-T0Threshold) > T0BestGuess)
+               fprintf(' ...But this is probably an error');
             end
             fprintf('\n');
-        else
-            dayMSeconds(idx) = -1;
-        end        
-        idx = idx + 1;
-        %fprintf([num2str(frameTime(frame,1)) ':' num2str(dec2bin(minorFrames(frame,9+1),8)) ' ' num2str(dec2bin(minorFrames(frame,10+1),8)) ' ' num2str(dec2bin(minorFrames(frame,11+1),8)) ' ' num2str(dec2bin(minorFrames(frame,12+1),8)) '\n']);
-        %fprintf([num2str(day) ' ' num2str(dec2hex(minorFrames(frame,8+1))) ' ' num2str(dec2hex(minorFrames(frame,9+1))) '\n\n']);        
     end
 end
 
